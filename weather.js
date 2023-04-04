6f8cd6d0df6e89f9d7e4ee78c490da39eb75451e
Cihat Kösem <44941726+CihatKsm@users.noreply.github.com>
2023-04-04 18:49:21 +0300

    .

--- weather.js
+++ weather.js
@@ -5,11 +5,12 @@ module.exports = async (datas) => await api(datas);
 
 setTimeout(async () => await checkUpdate(), 1000);
 async function checkUpdate() {
+    const module = 'weather-turkey'
     const package = require('./package.json');
-    const url = `https://unpkg.com/${package.name}@latest`;
+    const url = `https://unpkg.com/${module}@latest`;
     const api = await axios({ method: 'get', url }).catch((e) => null);
     const latest = api.request.path.split('/')[1].split('@')[1] || 0;
     const update = Number(latest.split('.').join('')) > Number(package.version.split('.').join(''))
-    const logText = `✅ Please update ${package.name} module ${package.version} to ${latest} version.`
+    const logText = `✅ Please update ${module} module ${package.version} to ${latest} version.`
     if (update) console.log('\x1b[32m%s\x1b[0m', logText)
 }
